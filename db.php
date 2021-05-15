e6cd8b6b578f604fa2d366261f0218ec3b4bd5b1
Nguyễn Văn Nhẫn <74455034+nguyenvannhan-dev@users.noreply.github.com>
2021-05-15 20:12:38 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = 'us-cdbr-east-03.cleardb.com';
 $username = 'b1e6e96208af97';
-$password = '0a4832ec';
-$dbname = 'heroku_65185c86ef41e4b';
+$password = '0a4832**';
+$dbname = 'heroku_65185c86ef41e**';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname)
